ddd75434203bb1247f11a46da0bdad57bcbf2a5e
peternikitakis <peternikitakis@gmail.com>
2023-11-22 13:38:41 -0500

    finir

--- 3_sorting.py
+++ 3_sorting.py
@@ -28,3 +28,22 @@ li = len(cars)
 print(li)
 
 #3-8 
+destination_list = ['virginia', 'new york', 'massachusetts', 'utah', 'illinois']
+sorted_list = sorted(destination_list, key = str.lower)
+print(sorted_list) #new sorted list without modifying original list
+print(destination_list) #original list not affected 
+
+sorted_list.reverse() #reversed list without modifying original list
+print(sorted_list)
+print(destination_list) #still unaffected 
+
+destination_list.sort() #sorting and modifying list permanently 
+print(destination_list)
+
+destination_list.sort(reverse=True) #sorting and modifying list so it's reversed permanently 
+print(destination_list)
+
+#3-9
+length_list = len(destination_list)
+print (length_list)
+
