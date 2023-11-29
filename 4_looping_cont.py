05fee17233bba0fbeb8c638a538fc5391df6e425
peternikitakis <peternikitakis@gmail.com>
2023-11-28 21:43:06 -0500

    deleting file

---
+++ 4_looping_cont.py
@@ -0,0 +1,7 @@
+#slicing a list
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+
+#omit the first index - automatically starts at the beginning of the list
+print(players[:4])
