0b746592dd5f52ef7c4337740ab79f4e951a00f8
peternikitakis <peternikitakis@gmail.com>
2023-11-29 15:02:01 -0500

    new

---
+++ 5_if_statements.py
@@ -0,0 +1,12 @@
+#programming often involves examining a set of conditions and deciding which action to take based on those conditions. Python's if statement allows you to examine the current state of a program and respond appropriately to that state. 
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for i in cars:
+    if i == 'bmw':
+        print(i.upper())
+    else: 
+        print(i.lower())
+
+
+
