449cd80fd523975ebf8d5aa992b25240a6286143
peternikitakis <peternikitakis@gmail.com>
2023-12-03 00:13:24 -0500

    end of chapter

---
+++ 5_if_statements_3.py
@@ -0,0 +1,23 @@
+#ordinal numbers
+ordinal_numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+for i in ordinal_numbers: 
+    if i == "1":
+        print(f"{i}st")
+    elif i == "2":
+        print(f"{i}nd")
+    elif i == "3":
+        print(f"{i}rd")
+    elif i == "4":
+        print(f"{i}th")
+    elif i == "5":
+        print(f"{i}th")
+    elif i == "6":
+        print(f"{i}th")
+    elif i == "7":
+        print(f"{i}th")
+    elif i == "8":
+        print(f"{i}th")
+    elif i == "9":
+        print(f"{i}th")
+
