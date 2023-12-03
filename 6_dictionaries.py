083ae935a7c1c8c316b537ef0a97074cb38fa60f
peternikitakis <peternikitakis@gmail.com>
2023-12-03 00:33:19 -0500

    new file lets begin

---
+++ 6_dictionaries.py
@@ -0,0 +1,8 @@
+#In this chapter you'll learn how to use Python's dictionaries, which allow you to connect pieces of related information. You'll learn how to access the information once it's in a dictionary and how to modify that information. 
+
+#a simple dictionary (consider a game featuring aliens)
+alien_0 = {'color': 'green', 'points': 5}
+
+print(alien_0['color'])
+print(alien_0['points'])
+
