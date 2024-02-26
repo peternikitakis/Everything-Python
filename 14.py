7ed1c9b465bed92d99f078fd2037304718a911da
peternikitakis <peternikitakis@gmail.com>
2024-02-26 00:56:22 -0500

    my own version

--- 14.py
+++ 14.py
@@ -6,6 +6,7 @@ ties = 0
 
 while True: 
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    
     print("Hello Player, above this message you shall see your current leaderboard standing.")
     print("Would you like to continue? (Y / N)")
     playerDecision = input()
@@ -41,24 +42,32 @@ while True:
         if playerMove == computerMove: 
             print("You and the computer have tied!")
             ties = ties + 1
+            break
         elif playerMove == 'r' and computerMove == 's':
             print("You won!")
             wins = wins + 1 
+            break
         elif playerMove == 'p' and computerMove == 'r':
             print("You won!")
             wins = wins + 1
+            break
         elif playerMove == 's' and computerMove == 'p':
             print("You won!")
             wins = wins + 1 
+            break
         elif playerMove == 'r' and computerMove == 'p':
             print("You lost!")
             losses = losses + 1
+            break
         elif playerMove == 'p' and computerMove == 's':
             print("You lost!")
             losses = losses + 1
+            break
         elif playerMove == 's' and computerMove == 'r':
             print("You lost!")
             losses = losses + 1
+            break
+        
 
         
         
