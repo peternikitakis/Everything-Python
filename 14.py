aa5455c0bc62f36ec7806466833cea11cbf3505f
peternikitakis <peternikitakis@gmail.com>
2024-02-25 23:43:25 -0500

    yes

--- 14.py
+++ 14.py
@@ -7,14 +7,13 @@ ties = 0
 while True: 
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
     print("Hello Player, above this message you shall see your current leaderboard standing.")
-    while True:
-        print("Would you like to continue? (Y / N)")
-        playerDecision = input()
-        if playerDecision == 'Y':
-            continue
-        if playerDecision == 'N':
+    print("Would you like to continue? (Y / N)")
+    playerDecision = input()
+    if playerDecision == 'Y':
+         True
+    if playerDecision == 'N':
             sys.exit()
-            
+    while True:  
         print("Please enter one of the following below - r(ock), p(aper), s(cissors), q(uit)")
         playerMove = input()
         if playerMove == 'r':
@@ -27,26 +26,30 @@ while True:
             print("QUIT")
             sys.exit()
 
-        computerMove = random.randint(1,3)
-        if computerMove == 1:
+        randomNumber = random.randint(1,3)
+        if randomNumber == 1:
+            computerMove = 'r'
             print("ROCK")
-        elif computerMove == 2:
+        elif randomNumber == 2:
+            computerMove = 'p'
             print("PAPER")
-        elif computerMove == 3: 
+        elif randomNumber == 3: 
+            computerMove = 's'
             print("SCISSORS")
 
-        elif playerMove == computerMove: 
+    # display the results
+        if playerMove == computerMove: 
             print("You and the computer have tied!")
             ties = ties + 1
         elif playerMove == 'r' and computerMove == 's':
             print("You won!")
-            won = won + 1 
+            wins = wins + 1 
         elif playerMove == 'p' and computerMove == 'r':
             print("You won!")
-            won = won + 1
+            wins = wins + 1
         elif playerMove == 's' and computerMove == 'p':
             print("You won!")
-            won = won + 1 
+            wins = wins + 1 
         elif playerMove == 'r' and computerMove == 'p':
             print("You lost!")
             losses = losses + 1
