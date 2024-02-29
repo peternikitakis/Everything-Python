38afc85396001f52729e3fcaa323501324a1f772
peternikitakis <peternikitakis@gmail.com>
2024-02-28 19:04:16 -0500

    new

---
+++ 15.py
@@ -0,0 +1,12 @@
+def hello():  
+    print('Hello!')
+    print('Hola!')
+    print('Bonjour!')
+
+hello()
+
+def hello(name): # defining a function named hello which takes one parameter called name
+    print('Hello, ' + name) # printing 'Hello' followed by the value of the name parameter
+
+hello('Alice') # calling the hello function with the argument 'Alice"
+hello('Bob') # calling the hello function 
