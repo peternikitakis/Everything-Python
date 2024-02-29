84c571a6668edd8fe57a1b7156c5254a2b4677c0
peternikitakis <peternikitakis@gmail.com>
2024-02-28 19:25:30 -0500

    more

---
+++ 17.py
@@ -0,0 +1,6 @@
+def sayHello(name): # variables that have arguments assigned to them are parameters
+    print('Hello, ' + name)
+
+sayHello('AI') # calling the function to pass in the 'AI' string value. THIS IS AN ARGUMENT BEING PASSED  
+
+ 
